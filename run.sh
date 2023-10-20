2d49b09eaadb1002d1a95e0be5699ba223475bfb
msuad <1445617868@qq.com>
2023-10-21 03:03:08 +0800

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -10,8 +10,9 @@ sudo apt install apt-fast
 sudo apt-fast install build-essential git gcc cmake ninja ninja-build openocd vim -y
 
 #cubemx
+$GET https://www.st.com/content/ccc/resource/technical/software/sw_development_suite/group0/46/64/3f/1b/cb/ec/4d/10/stm32cubemx-lin-v6-9-2/files/stm32cubemx-lin-v6-9-2.zip/jcr:content/translations/en.stm32cubemx-lin-v6-9-2.zip \
+        -o ./resource/
 unzip ./resource/en.stm32cubemx-lin-v6-9-2.zip -d ./cubemx
-$GET https://www.st.com/content/ccc/resource/technical/software/sw_development_suite/group0/46/64/3f/1b/cb/ec/4d/10/stm32cubemx-lin-v6-9-2/files/stm32cubemx-lin-v6-9-2.zip/jcr:content/translations/en.stm32cubemx-lin-v6-9-2.zip
 sudo chmod -R 777 ./cubemx/Setup*
 
 #jb tool box
