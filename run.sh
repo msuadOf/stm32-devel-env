68fd9e1c1ff8344465ef29e6319390e8e39a1ce7
msuad <1445617868@qq.com>
2023-10-21 03:15:14 +0800

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ sudo snap install --classic code
 sudo add-apt-repository ppa:apt-fast/stable -y
 sudo apt update
 sudo apt install apt-fast
-sudo apt-fast install build-essential git gcc cmake ninja ninja-build openocd vim -y
+sudo apt-fast install build-essential git gcc cmake ninja ninja-build openocd vim fuse -y
 
 #cubemx
 $GET https://www.st.com/content/ccc/resource/technical/software/sw_development_suite/group0/46/64/3f/1b/cb/ec/4d/10/stm32cubemx-lin-v6-9-2/files/stm32cubemx-lin-v6-9-2.zip/jcr:content/translations/en.stm32cubemx-lin-v6-9-2.zip \
@@ -24,11 +24,16 @@ tar -zxvf ./jetbrains-toolbox-2.0.5.17700.tar.gz
 $GET https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
 tar -xjf gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
 mv gcc-arm-none-eabi-10.3-2021.10 ~/
-arm-none-eabi-gcc -v
-if [$?]; then
-    echo "export PATH=\$PATH:/home/`whoami`/gcc-arm-none-eabi-10.3-2021.10/bin" >> ~/.bashrc
+source ~/.bashrc
+if command -v arm-none-eabi-gcc >/dev/null 2>&1; then 
+  echo 'exists arm-none-eabi-gcc' 
+else 
+  echo 'no exists arm-none-eabi-gcc' 
+  echo "export PATH=\$PATH:/home/`whoami`/gcc-arm-none-eabi-10.3-2021.10/bin" >> ~/.bashrc
+  source ~/.bashrc
+  echo "arm-none-eabi-gcc Added"
 fi
-export PATH=$PATH:/home/`whoami`/gcc-arm-none-eabi-10.3-2021.10/bin
+
 
 #microsoft-edge
 $GET https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_118.0.2088.57-1_amd64.deb
